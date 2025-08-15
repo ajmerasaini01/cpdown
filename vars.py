2c265fc599b0a9add0996c74c369d05af0fa93bb
ajmerasaini01 <ajmerasaini01@gmail.com>
2025-08-15 12:21:41 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,7 +1,8 @@
-api_id = 35814071 #change_with_your_Api_id.
-api_hash = "80c231f0e8704995b8259d06965n4351" #change_with_your_Api_hash.
+api_id = 26331872 #change_with_your_Api_id.
+api_hash = "c93589620441707c37c5683a02eea54e" #change_with_your_Api_hash.
 bot_token = "7198600769:AAHfVi7qTaMw20HbpvPrF-fnP77-sWdOthI" #change_with_your_bot_token.
-auth_users = [999741495] #change_with_your_user_id.
-sudo_user = 1713924419 #change_with_your_user_id.
-log_channel = -1002154281532 #change_with_your_channel_id.
-txt_channel = -1002154281532 #change_with_your_channel_id.
+auth_users = [5298142728] #change_with_your_user_id.
+sudo_user = 5298142728 #change_with_your_user_id.
+log_channel = --1002576929320 #change_with_your_channel_id.
+txt_channel = --1002576929320 #change_with_your_channel_id.
+
